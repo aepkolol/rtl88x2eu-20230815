17fcc2e5849d17e03d06ab99e7b7d5bf34385d1c
Alex <aepko.lol@gmail.com>
2024-10-27 19:33:15 -0300

    .

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -11202,8 +11202,7 @@ int rtw_wiphy_register(struct wiphy *wiphy)
 
 	RTW_INFO(FUNC_WIPHY_FMT"\n", FUNC_WIPHY_ARG(wiphy));
 
-#if ( (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)) 
-     || defined(RTW_VENDOR_EXT_SUPPORT) )
+#if ((LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0) || defined(RTW_VENDOR_EXT_SUPPORT)))
 	rtw_cfgvendor_attach(wiphy);
 #endif
 
