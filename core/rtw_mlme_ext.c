52751651c28a6950465ad3da7610f2cafc431a4e
Alex <aepko.lol@gmail.com>
2024-10-27 23:44:24 -0300

    ,

--- core/rtw_mlme_ext.c
+++ core/rtw_mlme_ext.c
@@ -11579,7 +11579,7 @@ static void rtw_mlmeext_disconnect(_adapter *padapter)
 		self_action = MLME_ADHOC_STOPPED;
 	else {
 		RTW_INFO("state:0x%x\n", MLME_STATE(padapter));
-		//rtw_warn_on(1);
+		rtw_warn_on(1);
 	}
 
 	/* set_opmode_cmd(padapter, infra_client_with_mlme); */
@@ -15720,42 +15720,49 @@ u8 rtw_iqk_hdl(_adapter *padapter, unsigned char *pbuf)
 
 u8 rtw_set_chbw_hdl(_adapter *padapter, u8 *pbuf)
 {
-    struct set_ch_parm *set_ch_parm;
-    struct mlme_ext_priv *pmlmeext = &padapter->mlmeextpriv;
-    u8 u_ch, u_bw, u_offset;
-    struct dvobj_priv *dvobj = adapter_to_dvobj(padapter);
+	struct set_ch_parm *set_ch_parm;
+	struct mlme_ext_priv	*pmlmeext = &padapter->mlmeextpriv;
+	u8 ifbmp_s = rtw_mi_get_ld_sta_ifbmp(padapter);
+	struct dvobj_priv *dvobj = adapter_to_dvobj(padapter);
+	u8 u_ch, u_bw, u_offset;
 
-    if (!pbuf)
-        return H2C_PARAMETERS_ERROR;
+	if (!pbuf)
+		return H2C_PARAMETERS_ERROR;
 
-    set_ch_parm = (struct set_ch_parm *)pbuf;
+	set_ch_parm = (struct set_ch_parm *)pbuf;
 
-    RTW_INFO(FUNC_NDEV_FMT " Received ch:%u, bw:%u, offset:%u\n",
-             FUNC_NDEV_ARG(padapter->pnetdev),
-             set_ch_parm->ch, set_ch_parm->bw, set_ch_parm->ch_offset);
+	RTW_INFO(FUNC_NDEV_FMT" ch:%u, bw:%u, ch_offset:%u\n",
+		 FUNC_NDEV_ARG(padapter->pnetdev),
+		 set_ch_parm->ch, set_ch_parm->bw, set_ch_parm->ch_offset);
 
-    /* Prevent the union logic from overriding settings */
-    pmlmeext->cur_channel = set_ch_parm->ch;
-    pmlmeext->cur_bwmode = set_ch_parm->bw;
-    pmlmeext->cur_ch_offset = set_ch_parm->ch_offset;
+	/*  update ch, bw, offset for all asoc STA ifaces */
+	if (ifbmp_s) {
+		_adapter *iface;
+		int i;
 
-    RTW_INFO("Calling set_channel_bwmode with ch:%u, offset:%u, bw:%u\n",
-             set_ch_parm->ch, set_ch_parm->ch_offset, set_ch_parm->bw);
-    set_channel_bwmode(padapter, set_ch_parm->ch, set_ch_parm->ch_offset, set_ch_parm->bw);
+		for (i = 0; i < dvobj->iface_nums; i++) {
+			iface = dvobj->padapters[i];
+			if (!iface || !(ifbmp_s & BIT(iface->iface_id)))
+				continue;
+			
+			/* update STA mode ch/bw/offset */
+			iface->mlmeextpriv.cur_channel = set_ch_parm->ch;
+			iface->mlmeextpriv.cur_bwmode = set_ch_parm->bw;
+			iface->mlmeextpriv.cur_ch_offset = set_ch_parm->ch_offset;
+			/* updaet STA mode DSConfig , ap mode will update in rtw_change_bss_chbw_cmd */
+			iface->mlmepriv.cur_network.network.Configuration.DSConfig = set_ch_parm->ch;
+		}
+	}
+	
+	LeaveAllPowerSaveModeDirect(padapter);
+	
+	set_channel_bwmode(padapter, set_ch_parm->ch, set_ch_parm->ch_offset, set_ch_parm->bw);
 
-    /* Re-sync interface channel information after setting */
-    rtw_mi_get_ch_setting_union(padapter, &u_ch, &u_bw, &u_offset);
-    RTW_INFO("Updated union settings - u_ch:%u, u_bw:%u, u_offset:%u\n", u_ch, u_bw, u_offset);
-
-    if (u_ch != set_ch_parm->ch) {
-        RTW_WARN("Union mismatch! Setting channel again to %u\n", set_ch_parm->ch);
-        pmlmeext->cur_channel = set_ch_parm->ch;
-        set_channel_bwmode(padapter, set_ch_parm->ch, set_ch_parm->ch_offset, set_ch_parm->bw);
-    }
-
-    rtw_rfctl_update_op_mode(dvobj_to_rfctl(dvobj), 0, 0);
-
-    return H2C_SUCCESS;
+	(void) rtw_mi_get_ch_setting_union(padapter, &u_ch, &u_bw, &u_offset);
+	rtw_mi_update_union_chan_inf(padapter, u_ch, u_offset, u_bw);
+	rtw_rfctl_update_op_mode(dvobj_to_rfctl(dvobj), 0, 0);
+	
+	return	H2C_SUCCESS;
 }
 
 u8 led_blink_hdl(_adapter *padapter, unsigned char *pbuf)
